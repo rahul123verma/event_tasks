62eb11614248ebc88b55f36b0a00012ce554d9c8
rahul <rahul.verma.2k11@gmail.com>
2016-12-07 22:10:26 +0530

    Skeleton

---
+++ application/views/home.php
@@ -0,0 +1,22 @@
+<?=$header?>
+<p>
+    Text Text Text Text Text Text Text Text Text Text Text Text Text 
+    Text Text Text Text Text Text Text Text Text Text Text Text Text 
+    Text Text Text Text Text Text Text Text Text Text Text Text Text 
+    Text Text Text Text Text Text Text Text Text Text Text Text Text 
+</p>
+
+<p>
+    Text Text Text Text Text Text Text Text Text Text Text Text Text 
+    Text Text Text Text Text Text Text Text Text Text Text Text Text 
+    Text Text Text Text Text Text Text Text Text Text Text Text Text 
+    Text Text Text Text Text Text Text Text Text Text Text Text Text 
+</p>
+
+<p>
+    Text Text Text Text Text Text Text Text Text Text Text Text Text 
+    Text Text Text Text Text Text Text Text Text Text Text Text Text 
+    Text Text Text Text Text Text Text Text Text Text Text Text Text 
+    Text Text Text Text Text Text Text Text Text Text Text Text Text 
+</p>
+<?=$footer?>
